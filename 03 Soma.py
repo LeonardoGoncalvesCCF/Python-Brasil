82237f38e49428e363cafe7ae76136c6b71cc127
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2025-05-26 15:43:24 -0300

    Exercício 04

--- 03 Soma.py
+++ 03 Soma.py
@@ -4,5 +4,6 @@ numero = float(input("Digite o primeiro numero: "))
 numero2 = float(input("Digite o segundo numero: "))
 
 soma = numero + numero2 
-
+print("---------------------------------")
 print(f" Asoma de {numero} e {numero2} é igual a {soma}")  
+print("---------------------------------")
---
+++ 04 Media.py
@@ -0,0 +1,11 @@
+print("---------------------------------")
+
+nota = float(input("Digite o primeira nota: "))
+nota2 = float(input("Digite o segunda nota: "))
+nota3 = float(input("Digite o terceira nota: "))
+nota4 = float(input("Digite o quarta nota: "))
+
+media = (nota + nota2 + nota3 + nota4)/4
+print("---------------------------------")
+print("A média das notas do aluno é: ", media)  
+print("---------------------------------")
