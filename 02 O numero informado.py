d5a940df386b4910b817ad4035c84e4295773f1f
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2025-05-26 15:31:55 -0300

    Exerícios 1 e 2

---
+++ 02 O numero informado.py
@@ -0,0 +1,7 @@
+print("---------------------------------")
+
+numero = input(" Digite um numero : ")
+
+print("---------------------------------")
+print(f" O numero ifnroamdo foi {numero}")   
+print("---------------------------------")
