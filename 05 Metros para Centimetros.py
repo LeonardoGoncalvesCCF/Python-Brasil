64cc4b8aa7c9ba197165a2fe465f0fa6f848cdc4
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2025-05-26 15:54:44 -0300

    Exercícios 5 e 6

---
+++ 05 Metros para Centimetros.py
@@ -0,0 +1,9 @@
+print("---------------------------------")
+print(" Esse programa converte metros para centímetros ")
+print("---------------------------------")
+metros = float(input("Digite a quantidade de metros: "))
+
+centimetros = metros * 100
+print("---------------------------------")
+print(f"{metros} metros é igual a {centimetros} centímetros")
+print("---------------------------------")
---
+++ 06 Raio do Circulo.py
@@ -0,0 +1,9 @@
+print("---------------------------------")
+raio = float(input(" Digite o raio do círculo: "))
+
+diametro = raio * 2
+
+print("---------------------------------")
+print(" O diâmetro do círculo é: ", diametro)
+print(f" O diâmetro do círculo é: {diametro} ")
+print("---------------------------------")
