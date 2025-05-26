4145700ed59e50c9be9016a3646c294f8fb97044
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2025-05-26 15:37:43 -0300

    Exercicio 3

---
+++ 03 Soma.py
@@ -0,0 +1,8 @@
+
+print("---------------------------------")
+numero = float(input("Digite o primeiro numero: "))
+numero2 = float(input("Digite o segundo numero: "))
+
+soma = numero + numero2 
+
+print(f" Asoma de {numero} e {numero2} é igual a {soma}")  
