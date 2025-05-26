3138e90543467d7690d44c213484bcc3a3ea8345
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2025-05-26 16:03:03 -0300

    Exercício 07

---
+++ 07 Area do Quadrado.py
@@ -0,0 +1,11 @@
+print("---------------------------------")
+
+lado = float(input("Digite o valor do lado do quadrado: "))
+
+area = lado ** 2
+area_dobrada = area * 2
+
+print("---------------------------------")
+print(f"A área do quadrado é: {area} m²")
+print(f"A área do quadrado dobrada é: {area_dobrada} m²")
+print("---------------------------------")
