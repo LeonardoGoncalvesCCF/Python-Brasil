56c2cfb0c06997e6f281b442c797b5ff1fa173cb
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2025-05-27 15:11:32 -0300

    Exercício 13

---
+++ 13 Gigabytes para Kilobytes.py
@@ -0,0 +1,10 @@
+print("-----------------------------------------------------")
+gigabytes = float(input(" Digite um numero em Gigabytes: "))
+
+megabytes = gigabytes * 1024
+kilobytes = megabytes * 1024
+
+print("-----------------------------------------------------")
+
+print(f" {gigabytes} GB é igual a {megabytes} MB")
+print(f" {gigabytes} GB é igual a {kilobytes} KB") 
